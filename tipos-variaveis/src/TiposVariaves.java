1c1b7ff5ca056d7f60b74c4c2c31c06aae170777
José Alexandre <josealexandre301428@gmail.com>
2024-04-30 17:42:06 -0300

    anotaçoes e exemplos de tipos

---
+++ tipos-variaveis/src/TiposVariaves.java
@@ -0,0 +1,27 @@
+public class TiposVariaves {
+    public static void main(String[] args) throws Exception {
+        //constante, ao utilizar a palavra final, não pode ser alterada o valor 
+        final int NUMERO = 5;
+        // variaveis numericas
+        short idade = 21;
+        int numero2 = 5;
+        double salario = 2000;
+        long cpf = 12345678910L;
+        float pi = 3.14F;
+
+        //variaveis booleanas
+        boolean status = true;
+        boolean status2 = false;
+        boolean status3 = 1 > 2;
+        boolean status4 = 1 < 2;
+        boolean status5 = 1 == 2;
+        boolean status6 = 1 != 2;
+        boolean status7 = 1 >= 2;
+        boolean status8 = 1 <= 2;
+        //variaveis texto
+        String nome = "Maria";
+        String nome2 = "Joao";
+        
+
+    }
+}
