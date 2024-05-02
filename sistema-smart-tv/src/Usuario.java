4e9bfc308b70ded8b74812206ad06b5488277673
José Alexandre <josealexandre301428@gmail.com>
2024-05-02 13:26:57 -0300

    Realiza desafio de metodos

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,12 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("Tv ligada? " + smartTv.ligada);
+        System.out.println("Canal atual? " + smartTv.canal);
+        System.out.println("Volume atual? " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> Tv ligada? " + smartTv.ligada);
+    }
+}
---
+++ tipos-variaveis/src/Metodos.java
@@ -0,0 +1,15 @@
+public class Metodos {
+    public static void main(String[] args) {//Metodo main
+        System.out.println("Hello World");
+    }
+    //deve ser nomeado como verbo
+    //Seguir camelCase
+    // se o metodo nao retorna nada, deve ser void
+    //se o metodo retorna algo, deve ser do tipo do retorno
+    //metodos podem receber parametros
+    //parametros sao valores passados para o metodo
+
+    public int somar(int a, int b){//Metodo para somar, recebe parametros do tipo int e retorna um int
+        return a+b;
+    }
+}
